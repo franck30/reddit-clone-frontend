0b29025416ffd12fc27d8330ad2099f37d7b0683
tchuitiocarin <tchuitiocarin@gmail.com>
2021-05-29 23:25:52 +0100

    conigure the home and post service

---
+++ src/app/shared/post-model.ts
@@ -0,0 +1,11 @@
+export class PostModel {
+  id: number;
+  postName: string;
+  url: string;
+  description: string;
+  votecount: number;
+  userName: string;
+  subredditName: string;
+  commentCount: number;
+  duration: string;
+}
