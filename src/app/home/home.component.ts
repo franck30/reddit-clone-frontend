192e9077cfd1ad06bdbcb1c7b5b080ac4a76e956
tchuitiocarin <tchuitiocarin@gmail.com>
2021-06-08 19:44:55 +0100

    apply some style to the home component to prevent overlapping

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PostModel } from '../shared/post-model';
 import { PostService } from '../shared/post.service';
+import {faArrowUp, faArrowDown, faComments} from '@fortawesome/free-solid-svg-icons'
 
 @Component({
   selector: 'app-home',
@@ -9,6 +10,10 @@ import { PostService } from '../shared/post.service';
 })
 export class HomeComponent implements OnInit {
 
+
+  faArrowUp = faArrowUp;
+  faArrowDown = faArrowDown;
+  faComments = faComments;
 
   posts$: Array<PostModel> = [];
 
